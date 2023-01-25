8a56f8e0d940a389a9f382af8fa5bf119e0c2824
pearce-odegard <pearceodegard@gmail.com>
2023-01-25 17:08:25 -0600

    Created 7 tests, refactored hasBalancedBrackets

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static main.BalancedBrackets.hasBalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,45 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void testEmptyStringHasBalancedBrackets() {
+        // Test case ---> ""
+        // we expect that an empty string does have balanced brackets
+        assertTrue(hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testSinglePairOfBalancedBrackets() {
+        // Test case ---> "[]"
+        assertTrue(hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testTenPairsOfNestedBalancedBrackets() {
+        // Test case ---> "[[[[[[[[[[]]]]]]]]]]"
+        assertTrue(hasBalancedBrackets("[[[[[[[[[[]]]]]]]]]]"));
+    }
+
+    @Test
+    public void testClosingThenOpeningBracketReturnsFalse() {
+        // Test case ---> "]["
+        assertFalse(hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testMoreOpeningBracketsThanClosingBracketsReturnsFalse() {
+        // Test case ---> "[[[[[[[[[[[]]]"
+        assertFalse(hasBalancedBrackets("[[[[[[[[[[[]]]"));
+    }
+
+    @Test
+    public void testRandomOrderOfBalancedBracketsReturnsTrue() {
+        assertTrue(hasBalancedBrackets("[[][][[]][]]"));
+    }
+
+    @Test
+    public void testRandomBracketsReturnsFalse() {
+        assertFalse(hasBalancedBrackets("[][[]]]][[]]]][]]][]]][][][][][][]]][]]["));
+    }
 
 }
